4eb058a1f280d4324825f84594f068eed1f51439
Jhenil Parihar <jhenilparihar@gmail.com>
2023-08-19 11:57:11 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, jsonify
-import json
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 
-def getScheme():
-    URL = "https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=20-Jun-2023&todt=20-Jun-2023"
+def scrape_data():
+    today = (datetime.now() - timedelta(1)).strftime("%d-%b-%Y")
+    URL = f"https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt={today}&todt={today}"
     response = requests.get(f"{URL}")
     data = response.text
     data = data.split("\r\n")
     data_filterd = [i for i in data if i]
+    return data_filterd
+
+
+def getScheme():
+    data_filterd = scrape_data()
 
     keys = [
         "Scheme Code",
@@ -25,7 +31,6 @@ def getScheme():
     ]
     x = ""
     y = ""
-
     list_of_scheme = []
 
     for i in data_filterd[1:]:
@@ -43,16 +48,48 @@ def getScheme():
     return list_of_scheme
 
 
-def getSchemes():
-    with open("schemes.json") as json_file:
-        data = json.load(json_file)
-    return data
+def getPrice():
+    data_filterd = scrape_data()
+
+    keys = [
+        "Scheme Code",
+        "Scheme Name",
+        "ISIN Div Payout/ISIN Growth",
+        "ISIN Div Reinvestment",
+        "Net Asset Value",
+        "Repurchase Price",
+        "Sale Price",
+        "Date",
+    ]
+
+    list_of_scheme = []
+    for i in data_filterd[1:]:
+        if "Open Ended Schemes" in i:
+            x = i
+        elif ";" not in i:
+            y = i
+        else:
+            scheme = {}
+            for i, j in enumerate(i.split(";")):
+                if i == 0 or i > 3:
+                    if i < 7:
+                        if i == 0:
+                            scheme[keys[i]] = int(j)
+                        elif j != "":
+                            scheme[keys[i]] = float(j)
+                        else:
+                            scheme[keys[i]] = None
+                        continue
+                    scheme[keys[i]] = j
+            list_of_scheme.append(scheme)
+
+    return list_of_scheme
 
 
 @app.route("/api", methods=["GET"])
 def index():
-    return jsonify(getScheme())
+    return jsonify(getPrice())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
--- fetchPrice.py
+++ fetchPrice.py
@@ -1,7 +1,10 @@
 import requests
 import json
+from datetime import datetime, timedelta
 
-URL = "https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=31-Jul-2023&todt=31-Jul-2023"
+yesterday = (datetime.now() - timedelta(2)).strftime('%d-%b-%Y')
+today = (datetime.now() - timedelta(1)).strftime('%d-%b-%Y')
+URL = f"https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt={today}&todt={today}"
 response = requests.get(f"{URL}")
 data = response.text
 data = data.split("\r\n")
@@ -21,21 +24,28 @@ x = ""
 y = ""
 
 list_of_scheme = []
-
+cnt=0
 for i in data_filterd[1:]:
+    cnt += 18
     if "Open Ended Schemes" in i:
         x = i
     elif ";" not in i:
         y = i
     else:
-
-        scheme = {"Fund Name": y, "Category": x}
+        scheme = {}
         for i, j in enumerate(i.split(";")):
-            scheme[keys[i]] = j
-
+            if i == 0 or i > 3:
+                if i != 0 and i < 7:
+                    if j != '':
+                        scheme[keys[i]] = float(j)
+                    else:
+                        scheme[keys[i]] = None
+                    continue
+                scheme[keys[    i]] = j
         list_of_scheme.append(scheme)
 
 
 file_path = "schemes.json"
 file_ = open(file_path, "w")
 json.dump(list_of_scheme, file_)
+print(cnt)
